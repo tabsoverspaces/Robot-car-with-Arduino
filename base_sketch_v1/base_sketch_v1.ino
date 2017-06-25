4400ff56db3aaa6c21cdbb6dc8732d4d1816cd11
tabsoverspaces <d.postolovski@gmail.com>
2017-06-26 00:37:35 +0200

    Init commit v3

--- base_sketch_v1/base_sketch_v1.ino
+++ base_sketch_v1/base_sketch_v1.ino
@@ -26,39 +26,79 @@ const int rearLights = 12;
 // rathen than through one common pin
 // being able to tweak their rotation
 // can offer better stability while cornering
-const int rightDrive = 2;
-const int leftDrive = 3;
+// we need 2 pins per motor,
+// to be able to drive it forwards
+// as well as backwards
+const int rightDriveForward = 2;
+const int rightDriveBackward = 3;
+const int leftDriveForward = 4;
+const int leftDriveBackward = 5;
 
 // front servo motors that steer the robot
 // they have to be separately controlled
 // thus two output pins are needed
 // we also need two Servo instances
 // for controlling the motors
-const int rightSteer = 4;
-const int leftSteer = 5;
+const int rightSteer = 6;
+const int leftSteer = 7;
 Servo rightServo;
 Servo leftServo;
 
+
+// MAIN SETUP METHOD
 void setup() {
   // lights
   pinMode(frontLights, OUTPUT);
   pinMode(readLights, OUTPUT);
 
   // drive motors
-  pinMode(rightDrive, OUTPUT);
-  pinMode(leftDrive, OUTPUT);
+  pinMode(rightDriveForward, OUTPUT);
+  pinMode(rightDriveBackward, OUTPUT);
+  pinMode(leftDriveForward, OUTPUT);
+  pinMode(leftDriveBackward, OUTPUT);
 
   // steering servos
   rightServo.attach(rightSteer);
   leftServo.attach(leftSteer);
   
-  
- 
-
-
 }
 
+
+// MAIN LOOP METHOD
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+/////////////////////////////////////////////////
+// ADDITIONAL METHODS
+
+// method used to park the car,
+// i.e. set all engine movement to zero
+// both forward and backward
+// this might have unexpected behaviour
+// if stopping uphill or downhill
+void park()
+{
+  digitalWrite(rightDriveForward, LOW);
+  digitalWrite(leftDriveForward, LOW);
+  digitalWrite(rightDriveBackward, LOW);
+  digitalWrite(leftDriveBackward, LOW);
+}
+
+// method used to drive the car forwards
+// uses both motors equally
+// FOR NOW at full speed
+void drive()
+{
+  digitalWrite(rightDriveForward, HIGH);
+  digitalWrite(leftDriveForward, HIGH);
+}
+
+// method used to drive the car in reverse
+// both motors equally used
+void reverse()
+{
+  digitalWrite(rightDriveBackward, HIGH);
+  digitalWrite(leftDriveBackward, HIGH);
 }
